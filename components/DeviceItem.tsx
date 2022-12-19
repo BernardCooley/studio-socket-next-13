1450d199c892fc80060162033ba44dce9185a352
Bernard.Cooley <Bernard.Cooley@global.com>
2022-12-19 11:49:01 +0000

    device item

---
+++ components/DeviceItem.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { noop } from "../utils";
+import ImageWithFallback from "./ImageWithFallback";
+
+interface Props {
+    // TODO: Add device type
+    device: any;
+    onClick?: () => void;
+}
+
+const DeviceItem = ({ device, onClick = noop }: Props) => {
+    return (
+        <div
+            className="border-primary-light-border border-2 shadow-lg rounded-md"
+            onClick={onClick}
+        >
+            {Object.keys(device).length > 0 && (
+                <div className="p-2 h-full flex flex-col justify-between">
+                    <div>
+                        <div className="text-lg">
+                            {device.manufacturers.join(", ")}
+                        </div>
+                        <div className="text-2xl">{device.title}</div>
+                    </div>
+                    <div className="relative w-full aspect-square mt-2">
+                        <ImageWithFallback
+                            fit="cover"
+                            title={device.title}
+                            fallbackImage="/assets/images/deviceFallbackImage.png"
+                            image={{
+                                name: device.title,
+                                url: device.image,
+                            }}
+                        />
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default DeviceItem;
--- components/PageSection.tsx
+++ components/PageSection.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const PageSection = ({ children, title }: Props) => {
     return (
-        <div className="relative w-11/12 h-96 shadow-xl mt-10 pb-32 border-2 border-primary-light-border rounded-md">
+        <div className="grid gap-4 grid-cols-2 relative w-11/12 shadow-xl mt-10 p-4 pt-7 border-2 border-primary-light-border rounded-md">
             <div className="absolute left-1/2 -translate-x-1/2 -top-4 text-xl px-4 bg-primary-light text-primary border-primary-light-border border-2 rounded-full">
                 {title}
             </div>
