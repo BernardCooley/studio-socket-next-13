9ef8ec6cb75dbc16abc4dafb5ea057f7a214d52b
BernardCooley <bernardcooley@gmail.com>
2023-03-24 15:44:46 +0000

    chakra installed

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { FormContextProvider } from "../contexts/FormContext";
+import { ChakraProvider } from "@chakra-ui/react";
 
 interface Props {
     children: React.ReactNode;
@@ -18,13 +19,15 @@ export default function RootLayout({ children }: Props) {
             </Head>
             <SessionProvider>
                 <body>
-                    <FormContextProvider>
-                        <div
-                            className={`h-full min-h-screen bg-primary-light font-default`}
-                        >
-                            {children}
-                        </div>
-                    </FormContextProvider>
+                    <ChakraProvider>
+                        <FormContextProvider>
+                            <div
+                                className={`h-full min-h-screen bg-primary-light font-default`}
+                            >
+                                {children}
+                            </div>
+                        </FormContextProvider>
+                    </ChakraProvider>
                 </body>
             </SessionProvider>
         </html>
